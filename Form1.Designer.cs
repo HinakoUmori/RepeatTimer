90980ca903eee6e69106b9de434b3215011eff70
HinakoUmori <takumasano98@yahoo.co.jp>
2021-01-09 13:03:37 +0900

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace CountUpToTheLimit
+{
+    partial class RepeatTimer
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.txtLimit = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.Font = new System.Drawing.Font("MS UI Gothic", 16.06154F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblTime.Location = new System.Drawing.Point(13, 9);
+            this.lblTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(199, 56);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "label1";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtLimit
+            // 
+            this.txtLimit.Location = new System.Drawing.Point(18, 69);
+            this.txtLimit.Margin = new System.Windows.Forms.Padding(4);
+            this.txtLimit.Name = "txtLimit";
+            this.txtLimit.Size = new System.Drawing.Size(76, 30);
+            this.txtLimit.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(112, 69);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 32);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(112, 145);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(4);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(100, 32);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(112, 105);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(100, 32);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // RepeatTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 189);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtLimit);
+            this.Controls.Add(this.lblTime);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 12.18462F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "RepeatTimer";
+            this.Text = "RepeatTimer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.TextBox txtLimit;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CountUpToTheLimit
+{
+    public partial class RepeatTimer : Form
+    {
+        DateTime pv_start; //DateTime when btnStart is pressed
+        DateTime pv_stop; //DateTIme when btnStop is pressed
+        TimeSpan span; //Validable to hold timespan temporary
+        TimeSpan nowspan; //TimeSpan from latest press of btnStart
+        TimeSpan pastspan; //TimeSpan by pressing btnStop
+        TimeSpan show; //TimeSpan to show
+        TimeSpan limit; //Valiable to hold the value of txtLimit
+
+        public RepeatTimer()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            nowspan = DateTime.Now - pv_start;
+            show = nowspan + pastspan;
+            if (show > limit)
+            {
+                lblTime.Text = "00:00:00";
+                span = DateTime.Now - DateTime.Now;
+                nowspan = span;
+                pastspan = span;
+                show = span;
+                pv_start = DateTime.Now;
+            }
+            lblTime.Text = show.ToString(@"mm\:ss\:ff");
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int tmpLimit = 0;
+            try
+            {
+                pv_start = DateTime.Now;
+                tmpLimit = int.Parse(txtLimit.Text.Trim());
+                limit = new TimeSpan(0, 0, tmpLimit);
+                txtLimit.Enabled = false;
+                timer1.Enabled = true;
+                btnStart.Enabled = false;
+                btnStop.Enabled = true;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            pv_stop = DateTime.Now;
+            span = pv_stop - pv_start;
+            pastspan += span;
+            txtLimit.Enabled = true;
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            lblTime.Text = "00:00:00";
+            span = DateTime.Now - DateTime.Now;
+            nowspan = span;
+            pastspan = span;
+            txtLimit.Enabled = true;
+            show = span;
+            pv_start = DateTime.Now;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartPosition = FormStartPosition.CenterScreen;
+            lblTime.Text = "00:00:00";
+            pastspan = DateTime.Now - DateTime.Now;
+            btnStop.Enabled = false;
+        }
+
+
+    }
+}
